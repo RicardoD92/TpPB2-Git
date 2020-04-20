b8c571786dab64f78071fb7e4aca042ef856e9b8
RicaroD92 <duchiarg@gmail.com>
2020-04-20 19:15:47 -0300

    Commit proyecto PB 2 javagit TP

---
+++ src/Ahorcado.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Ahorcado {
+	public static void main(String[] args) {
+	Integer a;
+	Integer b;
+	Integer opcion;
+	Integer resultado;
+	Scanner teclado= new Scanner(System.in);
+		do{
+			System.out.println("*****************************");
+			System.out.println("Ingresar su opncion:");System.out.println("--------------------");
+			System.out.println("1 pasa sumar");
+			System.out.println("2 para restar");
+			System.out.println("3 para multiplicar");
+			System.out.println("4 2para dividir");
+			System.out.println("");
+			System.out.println("*****************************");
+		
+		
+		opcion = teclado.nextInt();
+			}while(opcion < 1 && opcion >4);
+		System.out.println("Ingresar el primer numero");
+		a = teclado.nextInt();
+		System.out.println("Ingresar el segundo numero numero");
+		b = teclado.nextInt();
+		if(opcion.equals(1)) {
+			resultado = a + b;	
+		}else if (opcion.equals(2)){
+			resultado = a + b;
+		}else if (opcion.equals(3)) {
+			resultado = a * b;
+		}else {
+			resultado = a / b;
+		}
+		System.out.println("El resultado es " + resultado.toString());
+		
+	}
+}
+ 
